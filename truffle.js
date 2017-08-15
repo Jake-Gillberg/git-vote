eb6f885a9a26057f4b7a28575e303fe6f4587f84
Jake Gillberg <jake.gillberg@gmail.com>
2017-08-15 04:16:10 +0000

    migrate to ropsten

--- truffle.js
+++ truffle.js
@@ -1,5 +1,13 @@
 module.exports = {
   networks: {
+    ropsten: {
+      host: "localhost",
+      port: 8545,
+      network_id: 3,
+      gasPrice: 20000000000,
+      gas: 1000000,
+      from: '0x481b4048534290f3ee3ce5166d1a2eb1429238b2'
+    },
     development: {
       host: "localhost",
       port: 8545,
